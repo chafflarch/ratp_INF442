d9843b8917554b75cac1a0107f9a2b715094425b
Charles Benichou chaffanjon <charlesbenichouchaffanjon@MacBook-Air-de-Charles-2.local>
2024-03-19 15:00:16 +0100

    little modif on pandaframe returned by exploring_delays

--- exploring_delays.py
+++ exploring_delays.py
@@ -28,18 +28,30 @@ head = {'apiKey' : api_token , 'Date': request_time}
 
 
 def real_arrival_times(delta_t):  # delta_t : temps en seconde pendant lequel on regarde tous les horaires d'arrivée exacts ; 
+
+    # sort un dataframe à trois colonnes : la ref du train (que l'on peut lier à l'horaire prévu) , l'horaire prévu 30min avant et l'horaire réel d'arrivée
+    df = pd.DataFrame(columns=['VehicleJourneyName', 'forcasted_arrival_time30M' , 'arrival_time'])
+
+    arrival_times = []
     t =0
     while(t<= delta_t):
         response = requests.get(api_url, headers=head)
         data = json.loads(response.content)
+
         first_vehicle_at_stop =  [visit["MonitoredVehicleJourney"]["MonitoredCall"]["VehicleAtStop"] for visit in data["Siri"]["ServiceDelivery"]["StopMonitoringDelivery"][0]["MonitoredStopVisit"]][0]
         if(first_vehicle_at_stop):
                 # On stocke la ref du train et l'horaire de la première requête qui renvoie un first_vehicle_at_stop True : 
                 # On prend l'horaire d'arrivée réel - l'horaire d'arrivée prévu comme le retard dans cette convention ; 
-            
+                # il n'y a pas vraiment de ref du train
+                #Ce que l'on peut faire c'est prendre l'horaire prévu 1h avant... (ce qui est plutôt réaliste avec l'expérience voyageur)
+                # ça évite de croiser avec le planning prévu plusieurs jours à l'avance et qui ne correspond à rien pour les voyageurs du RER B 
+
+                # on a       "VehicleJourneyName": [{"value": "PIST76"}] qui a l'air bien fait pour 1journée
+                pass
 
         time.sleep(10) 
         t+=10
+
 
 
 real_arrival_times(30)
