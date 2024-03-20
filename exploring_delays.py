b7dc93d49b21cfc4d983a653c628bac6f68e2262
Charles Benichou chaffanjon <charlesbenichouchaffanjon@MacBook-Air-de-Charles-2.local>
2024-03-20 18:48:12 +0100

    bettering delays

--- exploring_delays.py
+++ exploring_delays.py
@@ -4,54 +4,104 @@ import json
 import time
 import pytz
 from datetime import datetime, timedelta
+
+
 # On a à disposion l'ensemble du planning très à l'avance (30j) actualisé trois fois par jour 
 #: https://data.iledefrance-mobilites.fr/explore/dataset/offre-horaires-tc-gtfs-idfm/information/
 # Mais ce n'est pas vraiment avec cela que l'on va avoir la réalité des délais engendrés pour un passager... 
+ # ça évite de croiser avec le planning prévu plusieurs jours à l'avance et qui ne correspond à rien pour les voyageurs du RER B
+
+''' 
+    1ère approche : On regarde 30 min avant l'horaire annoncé d'un train, puis on attend d'observer son arrivée réelle
+    Cela correspond assez bien à l'expérience utilisateur
+    Il y a peut-être une base de donnée qui compile des données historiques comparables, mais je n'ai pas encore trouvé
+    Pour l'instant, on le fait en temps réel sur l'API
+'''
+####################################################################################################
+# GOAL 2 : récupérer tous les ['VehicleJourneyName', 'forcasted_arrival_time30M' , 'arrival_time'] du RER B à Lozère vers CDG pendant une période de temps : 
+# on identifie un train à VehicleJourneyName (VJN) : e.g. [{"value": "PIST76"}] identifiant unique dans la journée (?) 
+####################################################################################################
 
 
-#obj : récupérer tous les horaires d'arrivée réels du RER B à Lozère pendant une période de temps : 
-
-
-
-# la base de la requête : 
-arret = "Lozère"
-csv_file = '/Users/charlesbenichouchaffanjon/Documents/Poly/Code/projet_INF442/csv_stops'
-df = pd.read_csv(csv_file, sep=';')
-print(df.loc[(df['ArRName'] == arret) & (df['ArRType'] == "rail") ])
+# la base de la requête : pour Lozère vers  CDG i.e. 473931
 api_token = '2Btko3bnqjqsyYIE6g8rJX7YwxjJ8pua'
-api_url = 'https://prim.iledefrance-mobilites.fr/marketplace/stop-monitoring?MonitoringRef=STIF%3AStopPoint%3AQ%3A474069%3A'
+api_url = 'https://prim.iledefrance-mobilites.fr/marketplace/stop-monitoring?MonitoringRef=STIF%3AStopPoint%3AQ%3A473931%3A'
 paris_timezone = pytz.timezone('Europe/Paris')
 paris_time = datetime.now(paris_timezone)
 request_time = paris_time.strftime('%Y-%m-%dT%H:%M:%S')
 head = {'apiKey' : api_token , 'Date': request_time}
 
 
-
 def real_arrival_times(delta_t):  # delta_t : temps en seconde pendant lequel on regarde tous les horaires d'arrivée exacts ; 
 
     # sort un dataframe à trois colonnes : la ref du train (que l'on peut lier à l'horaire prévu) , l'horaire prévu 30min avant et l'horaire réel d'arrivée
-    df = pd.DataFrame(columns=['VehicleJourneyName', 'forcasted_arrival_time30M' , 'arrival_time'])
 
+    df = pd.DataFrame(columns=['VehicleJourneyName', 'forcasted_arrival_time30M' , 'arrival_time'])
+    count = 0 #pour le nombre de request
     arrival_times = []
     t =0
     while(t<= delta_t):
+        
+        
         response = requests.get(api_url, headers=head)
         data = json.loads(response.content)
 
-        first_vehicle_at_stop =  [visit["MonitoredVehicleJourney"]["MonitoredCall"]["VehicleAtStop"] for visit in data["Siri"]["ServiceDelivery"]["StopMonitoringDelivery"][0]["MonitoredStopVisit"]][0]
-        if(first_vehicle_at_stop):
-                # On stocke la ref du train et l'horaire de la première requête qui renvoie un first_vehicle_at_stop True : 
-                # On prend l'horaire d'arrivée réel - l'horaire d'arrivée prévu comme le retard dans cette convention ; 
-                # il n'y a pas vraiment de ref du train
-                #Ce que l'on peut faire c'est prendre l'horaire prévu 1h avant... (ce qui est plutôt réaliste avec l'expérience voyageur)
-                # ça évite de croiser avec le planning prévu plusieurs jours à l'avance et qui ne correspond à rien pour les voyageurs du RER B 
+        ##################################
+        # check for new train to add to the list
+        # désolé la ligne est affreuse
+        current_VJNs = [visit["MonitoredVehicleJourney"]["VehicleJourneyName"][0]["value"] 
+                        for visit in data["Siri"]["ServiceDelivery"]["StopMonitoringDelivery"][0]["MonitoredStopVisit"] 
+                        if ("AEROPORT CH.DE GAULLE 2-MITRY CLAYE" in [direction["value"] for direction in visit["MonitoredVehicleJourney"]["DirectionName"]])]
+        
+        new_VJNs = [vjn for vjn in current_VJNs if vjn not in df['VehicleJourneyName'].values]
+        # Create DataFrame with new values and placeholder values
+        new_rows = pd.DataFrame({'VehicleJourneyName': new_VJNs,
+                         'forcasted_arrival_time30M': [None] * len(new_VJNs),
+                         'arrival_time': [None] * len(new_VJNs)})
+        df = pd.concat([df, new_rows], ignore_index=True)
 
-                # on a       "VehicleJourneyName": [{"value": "PIST76"}] qui a l'air bien fait pour 1journée
-                pass
 
+        ##################################
+        #check for the 'forcasted_arrival_time30M' of those that aren't yet defined
+        # Iterate over DataFrame rows where 'forcasted_arrival_time30M' is None
+        # It is supposed to work (maybe a mistake in the then-now... )
+        now = paris_time
+        for index, row in df[df['forcasted_arrival_time30M'].isnull()].iterrows():
+            row_VJN = row[0]
+            expected_arrival_time = [visit["MonitoredVehicleJourney"]["MonitoredCall"]["ExpectedArrivalTime"] 
+                                     for visit in data["Siri"]["ServiceDelivery"]["StopMonitoringDelivery"][0]["MonitoredStopVisit"] 
+                                     if (visit["MonitoredVehicleJourney"]["VehicleJourneyName"][0]["value"]==row_VJN)]
+            print(expected_arrival_time)
+            # check of expected arrival_time isn't empty : 
+            # sometimes it is apparently, at the moment the first VJN value disappears i.e. when a train arrives and if it has no forcasted_arrival_time30M
+            if(expected_arrival_time!=[]):
+                then = datetime.fromisoformat(expected_arrival_time[0]).astimezone(paris_timezone)
+                time_difference = then - now
+                #we check 30 minutes before expected_arrival
+                # and we want to be sure the prediction is really about 30min before and not less
+                if ((time_difference.total_seconds() < 30 * 60) & (time_difference.total_seconds() > 29 * 60) ): # DEBUG 
+                    df.loc[index , 'forcasted_arrival_time30M'] = then
+                 
+
+        ##################################
+        #check if first train in stack at stop 
+        first_vehicle_at_stop =  [visit["MonitoredVehicleJourney"]["MonitoredCall"]["VehicleAtStop"] 
+                                  for visit in data["Siri"]["ServiceDelivery"]["StopMonitoringDelivery"][0]["MonitoredStopVisit"]]
+        # check if this list isn't void
+        if( (first_vehicle_at_stop!=[]) & first_vehicle_at_stop[0] ): 
+                # get the VJNname of it  : 
+                first_VJN = [visit["MonitoredVehicleJourney"]["VehicleJourneyName"][0]["value"] 
+                                for visit in data["Siri"]["ServiceDelivery"]["StopMonitoringDelivery"][0]["MonitoredStopVisit"]][0]
+                
+                df.loc[df['VehicleJourneyName'] == first_VJN, 'arrival_time'] = request_time
+
+        # affichage en console pour le débugage 
+        print("df après " + str(count) + " requests")
+        print(df)
         time.sleep(10) 
         t+=10
+        count+=1
 
 
 
-real_arrival_times(30)
+real_arrival_times(7200)
--- exploring_stops.py
+++ exploring_stops.py
@@ -5,65 +5,81 @@ import json
 from datetime import datetime, timedelta
 import pytz
 
-# first goal : get the current time arrival and departure at Lozère for the RERB
+
+
+
+#################################################################################
+# GOAL 1 : get the current time arrival and departure at Lozère for the RERB with direction and VehicleJourneyName and know if it is at the stop
+# Sur Unitary-Real-Time, la limite est à 1 000 000  request par jour, donc on est large, mais dans Global-Real-Time,
+#si l'on vient à l'utiliser, on descent à 1 000  request par jour
+#################################################################################
 
 arret = "Lozère"
 #correspondance avec arrêt Id
 csv_file = '/Users/charlesbenichouchaffanjon/Documents/Poly/Code/projet_INF442/csv_stops'
-# préciser le séparateur est essentiel sinon il fait des erreurs sur certaines lignes
-df = pd.read_csv(csv_file, sep=';')
-print(df.loc[(df['ArRName'] == arret) & (df['ArRType'] == "rail") ])
+df = pd.read_csv(csv_file, sep=';')  # préciser le séparateur est essentiel sinon il fait des erreurs sur certaines lignes
+
+#To get the refs
+#print(df.loc[(df['ArRName'] == arret) & (df['ArRType'] == "rail") ])
 # on se retrouve avec trois ID différents pour Lozère rail ... Possiblement dans un sens, dans l'autre et les deux à la gare
-# deux sont en ArRFareZone = 4
-# appel à la base de donnée ; 
-# 474063 a l'air d'être vers Saint-Rémy
+# 474063 vers Saint-Rémy
 # 473931 vers CdG
-# 474069 est un arrêt fictif qui chapeaute les deux : 
+# 474069 est un arrêt fictif qui chapeaute les deux 
 
-# token for this API 2Btko3bnqjqsyYIE6g8rJX7YwxjJ8pua (
+
+'''
+pour automatiser encore plus, ce serait bien de passer de l'ArRName à l'ArRID en sachant déterminer quel est le bon sens sans tester la direction : 
+S'il y a une base de donnée qui référence cela, je ne l'ai pas trouvée : le sens des lignes existe-t-il dans PRIM ? 
+Pour l'instant, le plus simple est de garder les infos dans les deux sens, puis les trier
+'''
+
+
+
+# (TO BE HIDDEN)
 api_token = '2Btko3bnqjqsyYIE6g8rJX7YwxjJ8pua'
-api_url = 'https://prim.iledefrance-mobilites.fr/marketplace/stop-monitoring?MonitoringRef=STIF%3AStopPoint%3AQ%3A474069%3A'
 
-# Get the Paris timezone ; c'est un peu inutile mais on est sûr d'être en heure de Paris
+api_url = 'https://prim.iledefrance-mobilites.fr/marketplace/stop-monitoring?MonitoringRef=STIF%3AStopPoint%3AQ%3A473931%3A'
+# Get the Paris timezone ; Attention la base de donnée est en GMT
 paris_timezone = pytz.timezone('Europe/Paris')
 paris_time = datetime.now(paris_timezone)
 request_time = paris_time.strftime('%Y-%m-%dT%H:%M:%S')
 head = {'apiKey' : api_token , 'Date': request_time}
 response = requests.get(api_url, headers=head)
-
-# On veut : l'horaire auquel on a l'affichage  et les horaires prévus des prochains trains avec la direction
+ 
 # Parse JSON
 data = json.loads(response.content)
+# Extract the values from response
+expected_arrival_times = [visit["MonitoredVehicleJourney"]["MonitoredCall"]["ExpectedArrivalTime"] 
+                          for visit in data["Siri"]["ServiceDelivery"]["StopMonitoringDelivery"][0]["MonitoredStopVisit"]]
 
-# Extract ExpectedArrivalTime values
-expected_arrival_times = [visit["MonitoredVehicleJourney"]["MonitoredCall"]["ExpectedArrivalTime"] for visit in data["Siri"]["ServiceDelivery"]["StopMonitoringDelivery"][0]["MonitoredStopVisit"]]
+directions_packed = [visit["MonitoredVehicleJourney"]["DirectionName"] 
+                     for visit in data["Siri"]["ServiceDelivery"]["StopMonitoringDelivery"][0]["MonitoredStopVisit"]]
 
-directions_packed = [visit["MonitoredVehicleJourney"]["DirectionName"] for visit in data["Siri"]["ServiceDelivery"]["StopMonitoringDelivery"][0]["MonitoredStopVisit"]]
 directions =[train_direction[0]["value"] for train_direction in directions_packed]
+
+VJNs = [visit["MonitoredVehicleJourney"]["VehicleJourneyName"][0]["value"] 
+                        for visit in data["Siri"]["ServiceDelivery"]["StopMonitoringDelivery"][0]["MonitoredStopVisit"]]
 
 vehicle_at_stop =  [visit["MonitoredVehicleJourney"]["MonitoredCall"]["VehicleAtStop"] for visit in data["Siri"]["ServiceDelivery"]["StopMonitoringDelivery"][0]["MonitoredStopVisit"]]
 
-
 # Print the extracted values
+# On veut l'horaire auquel on a l'affichage 
 print("horaire du RER_B à l'arrêt Lozère (dans les deux sens) : demandé à  " + request_time)
 
 for i, str_time in enumerate(expected_arrival_times):
     #On remet dans le bon fuseau horaire : il faut le faire finement avec les décalages horaires
     # datatime.strptime prend le format de la date
-    GMT_time = datetime.strptime(str_time,'%Y-%m-%dT%H:%M:%S.%fZ')
-    # ajustement à l'heure de Paris
-    paris_timezone = pytz.timezone('Europe/Paris')
-    # il faut bien lui dire que l'horaire récupéré est en UTC
-    GMT_time = GMT_time.replace(tzinfo=pytz.utc)
-    Paris_time = GMT_time.astimezone(paris_timezone)
-    print(str(Paris_time) + ' direction ' + directions[i] + '  vehicle is at stop : ' + str(vehicle_at_stop[i])) # On assume qu'il n'y a qu'une direction par train !
+    # EAT for expected_arrival_times
+    GMT_EAT = datetime.strptime(str_time,'%Y-%m-%dT%H:%M:%S.%fZ')
+    # ajustement à l'heure de UTC à Paris
+    GMT_EAT = GMT_EAT.replace(tzinfo=pytz.utc)
+    Paris_EAT = GMT_EAT.astimezone(paris_timezone)
+    print("Paris_EAT = " + str(Paris_EAT)
+          + 'direction = ' + directions[i]
+          + '; VJN = ' + VJNs[i] 
+          + '; vehicle is at stop = ' + str(vehicle_at_stop[i]))  # On assume qu'il n'y a qu'une direction par train !
 
-# pour automatiser encore plus, ce serait bien de passer de l'ArRName à l'ArRID en sachant déterminer quel est le bon sens  : 
-# Attention c'est l'horaire GMT qui est renvoyé, il faut magouiller pour avoir le temps de paris en sortie
-# Correspondance Ligne arrêt possible ? 
-    # Zone d'arrêt : ZdA : c'est monomodal  , le ZdAId de Lozère est 474069 : évite de se tromper de sens : on garder les infos dans les deux sens, puis on peut trier  ;
-    # Zone de correspondance : ZdC : c'est plusieurs modes de transports regroupés
-    # J'ai l'impression qu'ils n'y a rien qui indique le sens dans lequel on peut bouger à partir d'un arrêt. 
+
 
 
 
